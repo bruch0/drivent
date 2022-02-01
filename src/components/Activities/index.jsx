7d9bc06a449311ffa0c7e591577b22a319776f42
bruch0 <lucas.bruch0@gmail.com>
2022-02-01 16:02:49 -0300

    fix: bug on receiving activities for the specific day

--- src/components/Activities/index.jsx
+++ src/components/Activities/index.jsx
@@ -1,27 +1,45 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import dayjs from "dayjs";
 import { ButtonsWrapper, ActivityButton } from "./ActivityButtons";
 import Schedule from "./Schedule";
 
 export default function ActivitiesSelection() {
-
-  const dates = ['04/02/2022', '05/02/2022', '06/02/2022'];
-  const [day, setDay] = useState('04/02/2022');
+  const dates = ["04/02/2022", "05/02/2022", "06/02/2022"];
+  const [day, setDay] = useState("2022/02/04");
   const [selected, setSelected] = useState(1);
 
   return (
     <>
       <ActivityMsg>Primeiro, filtre pelo dia do evento: </ActivityMsg>
       <ButtonsWrapper>
-          <ActivityButton onClick={() => {setDay(dates[0]); setSelected(1)}} selected={selected === 1}>
-            Sexta, 04/02
-          </ActivityButton>
-          <ActivityButton onClick={() => {setDay(dates[1]); setSelected(2)}} selected={selected === 2}>
-            Sábado, 05/02
-          </ActivityButton>
-          <ActivityButton onClick={() => {setDay(dates[2]); setSelected(3)}} selected={selected === 3}>
-            Domingo, 06/02
-          </ActivityButton>
+        <ActivityButton
+          onClick={() => {
+            setDay(dayjs(dates[0]).format("YYYY/DD/MM"));
+            setSelected(1);
+          }}
+          selected={selected === 1}
+        >
+          Sexta, 04/02
+        </ActivityButton>
+        <ActivityButton
+          onClick={() => {
+            setDay(dayjs(dates[1]).format("YYYY/DD/MM"));
+            setSelected(2);
+          }}
+          selected={selected === 2}
+        >
+          Sábado, 05/02
+        </ActivityButton>
+        <ActivityButton
+          onClick={() => {
+            setDay(dayjs(dates[2]).format("YYYY/DD/MM"));
+            setSelected(3);
+          }}
+          selected={selected === 3}
+        >
+          Domingo, 06/02
+        </ActivityButton>
       </ButtonsWrapper>
       <Schedule day={day} setDay={setDay} />
     </>

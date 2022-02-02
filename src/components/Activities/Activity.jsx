ed19a6ab6d7e2fbf053b6ca07607ff0613eea35f
rabbithay <thay.csb@gmail.com>
2022-02-02 19:45:59 -0300

    feat: subscribe activity on click

--- src/components/Activities/Activity.jsx
+++ src/components/Activities/Activity.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { toast } from "react-toastify";
 import { ActivityContainer, InfoContainer } from "./scheduleStyle";
 import convertTime from "./timeHandler";
 import VacancyIcon from "./VacancyIcon";
+import useApi from "../../hooks/useApi";
+
 
 export default function Activity({ duration, element }) {
+  const { activity } = useApi();
+
+  function subscribeActivity() {
+    activity.postNewActivity(element.id).then(() => {
+    }).catch((error) => {
+      if (error.response) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const detail of error.response.data.details) {
+          toast(detail);
+        }
+      } else {
+        toast("Não foi possível conectar ao servidor!");
+      }
+    })
+  }
+
   return (
-    <ActivityContainer duration={duration}>
+    <ActivityContainer duration={duration} onClick={() => subscribeActivity()}>
       <InfoContainer>
         <p>{element.name}</p>
         <span>

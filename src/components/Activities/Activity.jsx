c22e2403ff08e004002853249bb0d8b3b6712577
rabbithay <thay.csb@gmail.com>
2022-02-03 12:08:52 -0300

    feat: confirm schedule modal

--- src/components/Activities/Activity.jsx
+++ src/components/Activities/Activity.jsx
@@ -1,9 +1,11 @@
 import React, { useLayoutEffect, useState } from "react";
 import { toast } from "react-toastify";
+import Swal from 'sweetalert2'
 import { ActivityContainer, InfoContainer } from "./scheduleStyle";
 import convertTime from "./timeHandler";
 import VacancyIcon from "./VacancyIcon";
 import useApi from "../../hooks/useApi";
+
 
 
 export default function Activity({ duration, element }) {
@@ -21,14 +23,25 @@ export default function Activity({ duration, element }) {
   }, [])
 
   function subscribeActivity() {
-    activity.postNewActivity(element.id).then(() => {
-      setIsScheduled(true)
-      toast("Inscrito");
-    }).catch((error) => {
-      if (error.response.status === 409) {
-        toast("Não foi possível se inscrever: choque de horário!")
-      } else {
-        toast("Não foi possível conectar ao servidor!");
+    Swal.fire({
+      title: 'Você tem certeza que deseja se inscrever nessa atividade?',
+      text: "Essa ação não pode ser desfeita.",
+      showCancelButton: true,
+      confirmButtonColor: '#fa4697',
+      cancelButtonColor: '#ffd57f',
+      confirmButtonText: 'Me inscrever!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        activity.postNewActivity(element.id).then(() => {
+          setIsScheduled(true)
+          toast("Inscrito");
+        }).catch((error) => {
+          if (error.response.status === 409) {
+            toast("Não foi possível se inscrever: choque de horário!")
+          } else {
+            toast("Não foi possível conectar ao servidor!");
+          }
+        })
       }
     })
   }
